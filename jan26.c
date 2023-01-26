632e3da6b1a8ed009be14c595d03cc0975c30e24
Jyoti R Sulakhe <122368666+Jyotisulakhe@users.noreply.github.com>
2023-01-26 18:19:31 +0530

    Add files via upload

---
+++ jan26.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+int main(){
+    char ch;
+    printf("Enter");
+    scanf("%c",&ch);
+   if(ch=='J'){
+    printf("HAPPY REPUBLIC DAY");
+   }
+   else if(ch=='A'){
+        printf("HAPPY INDEPENDANCE DAY");
+   } else{
+       printf(":)");
+   }
+return 0;
+}
